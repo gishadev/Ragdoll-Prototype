34c549cde14e67b5670005f63c7b3351860dd645
Bohdan Tsaplia <b.tsaplia@gmail.com>
2022-01-03 21:53:24 +0200

    add shooter

---
+++ Ragdoll-Prototype/Assets/_Project/Scripts/Shooter.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private Transform shootPoint;
+    [SerializeField] private float shootForce = 20f;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            Shoot();
+    }
+
+    private void Shoot()
+    {
+        var proj = Instantiate(projectilePrefab, shootPoint.position, Quaternion.identity);
+
+        Ray shootRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var rigidbodies = proj.GetComponentsInChildren<Rigidbody>();
+
+        foreach (var rb in rigidbodies)
+            rb.AddForce(shootRay.direction * shootForce, ForceMode.Impulse);
+    }
+}
